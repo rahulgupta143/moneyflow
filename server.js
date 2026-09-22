47e715ea7045b65026d78f80596415c239e06043
Rahul Gupta <guptarahul1431@gmail.com>
2026-09-22 22:08:46 +0530

    Modify CORS settings to restrict access
    Updated CORS configuration to allow requests from a specific origin.

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://rahulgupta143.github.io"
+  })
+);
 app.use(express.json());
 
 /* ================= DATABASE ================= */
